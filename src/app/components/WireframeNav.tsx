4cc9e1bd887bf5f9d22ad3ec4658b5692b3ca434
Tanay1126 <tannaydesaii@gmail.com>
2026-03-10 18:25:56 -0700

    Added new accout creation jsx file, added get and post and organized componenets

--- src/app/components/WireframeNav.tsx
+++ src/app/components/WireframeNav.tsx
@@ -21,6 +21,8 @@ export function WireframeNav({ currentPage, userType, onPageChange, onUserTypeCh
     { id: 'sign-up' as PageType, label: 'Sign Up' },
   ];
 
+  
+
   const userPages = [
     { id: 'create-event' as PageType, label: 'Create Event' },
     { id: 'my-events' as PageType, label: 'My Events' },
